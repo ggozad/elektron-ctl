104f3d52c99e5d4a86e022b2cdedaf44d631477d
Jakob Penca <jakob@Jakobs-MacBook-Air.local>
2012-06-18 17:31:52 +0200

    Initial Commit

---
+++ MachineDrumFramework/MachineDrumFramework.h
@@ -0,0 +1,13 @@
+//
+//  MachineDrumFramework.h
+//  MachineDrumFramework
+//
+//  Created by Jakob Penca on 6/18/12.
+//  Copyright (c) 2012 Jakob Penca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MachineDrumFramework : NSObject
+
+@end
